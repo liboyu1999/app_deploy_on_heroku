ba7a2920b3503c7009821efc0e3dc33b017ed92d
liboyu1999 <90221816+liboyu1999@users.noreply.github.com>
2022-05-01 23:43:11 -0500

    Add files via upload

---
+++ app/config/config.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// api details
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+/*
+const firebaseConfig = {
+    apiKey: "AIzaSyDONyb_4F9cv1pqoN-5uOAJEPZci6RjZIw",
+    authDomain: "notification606-27489.firebaseapp.com",
+    projectId: "notification606-27489",
+    storageBucket: "notification606-27489.appspot.com",
+    messagingSenderId: "583648912304",
+    appId: "1:583648912304:web:c6fb45d9ea5a3522b16c75",
+    measurementId: "G-BCQ1HD1ZT0"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const f = firebase;
+export const database = firebase.database();
+export const auth = firebase.auth();
+export const storage = firebase.storage();
+*/
