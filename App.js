a581cf1502e41a89eb9a8ef1d371d4a1471176e2
liboyu1999 <90221816+liboyu1999@users.noreply.github.com>
2022-05-01 23:39:39 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+// import { store, persistor } from './app/redux/user';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './app/navigation/AuthNavigator';
+import Map from './app/screens/Map';
 import route from './app/navigation/routes';
+import {f, auth, database} from './app/config/config.js';
+import usersReducer from './app/redux/usersReducer';
 
+     const store = createStore(usersReducer);
 export default function App(){
   return(
-    <NavigationContainer>
-      <AuthNavigator/>
-    </NavigationContainer>
-  )
+    
+     <Provider store={store}>
+     <NavigationContainer>
+       <AuthNavigator/>
+    </NavigationContainer>  
+     </Provider>
+  );
 }
